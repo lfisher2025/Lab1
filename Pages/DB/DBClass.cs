97331d13390600a4c13b61215edb1f95bb794311
lfisher2025 <soccerfish03@gmail.com>
2025-02-21 18:07:51 -0500

    merging branch into main before bug fix

--- Pages/DB/DBClass.cs
+++ Pages/DB/DBClass.cs
@@ -7,6 +7,7 @@ namespace Lab1.Pages.DB
 {
     public class DBClass
     {
+        public string UserID { get; set; }
         // Use this class to define methods that make connecting to
         // and retrieving data from the DB easier.
 
@@ -23,36 +24,90 @@ namespace Lab1.Pages.DB
 
         //Add a new user
 
-        public static void AddUser(String firstName, String lastName, String middleInitial, String role, String email)
+        public static void AddUser(User newUser, int CurrentUserID)
         {
-            string addUserQuery = "INSERT INTO USERS (firstName, lastName, middleInitial, role, phoneNumber, email)" +
-                " VALUES (" + firstName + ", " + lastName + ", " + middleInitial + ", " + role + ", " + email + ");";
+            string addUserQuery = "INSERT INTO Users (firstName, lastName, middleInitial, role, phoneNumber, email) " +
+               "VALUES ('" + newUser.FirstName + "', '" + newUser.LastName + "', '" +
+               newUser.MiddleInitial + "', '" + newUser.UserType + "', '" +
+               newUser.PhoneNumber + "', '" + newUser.Email + "');"; ;
             SqlCommand cmdAddUser = new SqlCommand();
             cmdAddUser.Connection = Lab1DBConnection;
             cmdAddUser.Connection.ConnectionString = Lab1DBConnString;
             cmdAddUser.CommandText = addUserQuery;
             Lab1DBConnection.Open();
+            Lab1DBConnection.Close();
+
+            string getNewUserID = "SELECT userID FROM Users WHERE firstName = " + "'" + newUser.FirstName + "'" + ";";
+            SqlCommand cmdGetNewUserID = new SqlCommand();
+            cmdGetNewUserID.Connection = Lab1DBConnection;
+            cmdGetNewUserID.Connection.ConnectionString = Lab1DBConnString;
+            cmdGetNewUserID.CommandText = getNewUserID;
+            Lab1DBConnection.Open();
+
+            SqlDataReader IDreader = cmdGetNewUserID.ExecuteReader();
+
+            int IDResult = 0;
+            if (IDreader.Read())
+            {
+                IDResult = Convert.ToInt32(IDreader["userID"]);
+            }
+            Lab1DBConnection.Close();
+
+            if (newUser.UserType == "employee")
+            {
+                DBClass.AddEmployee(IDResult, CurrentUserID);
+            }
+            else if (newUser.UserType == "admin")
+            {
+                DBClass.AddAdmin(IDResult);
+            }
+            else if(newUser.UserType == "representative")
+            {
+                DBClass.AddRepresentative(IDResult);
+            }
+            else if( newUser.UserType == "faculty")
+            {
+                //will be added later, the initial form needs to dynamically change when a faculty user is selected to add new fields
+            }
 
 
         }
 
-        public static void AddEmployee()
-        {
-
+        public static void AddEmployee(int IDResult, int CurrentUserID)
+        { 
+            string addEmplyString = "INSERT INTO Employee (employeeID, adminID) VALUES ("+ IDResult + "," + CurrentUserID + ");";
+            SqlCommand cmdAddEmployee = new SqlCommand();
+            cmdAddEmployee.Connection = Lab1DBConnection;
+            cmdAddEmployee.Connection.ConnectionString = Lab1DBConnString;
+            cmdAddEmployee.CommandText = addEmplyString;
+            Lab1DBConnection.Open();
+            Lab1DBConnection.Close();
         }
-        public static void Addfaculty()
+        public static void Addfaculty(int IDResult)
         {
-
+            //will be added later, the initial form needs to dynamically change when a faculty user is selected to add new fields
         }
 
-        public static void AddRepresentative() 
+        public static void AddRepresentative(int IDResult) 
         {
+            string addRepString = "INSERT INTO Representative (representativeID) VALUES (" + IDResult + ");";
+            SqlCommand cmdAddRep = new SqlCommand();
+            cmdAddRep.Connection = Lab1DBConnection;
+            cmdAddRep.Connection.ConnectionString = Lab1DBConnString;
+            cmdAddRep.CommandText = addRepString;
+            Lab1DBConnection.Open();
+            Lab1DBConnection.Close();
+        } 
 
-        }
-
-        public static void AddAdmin()
+        public static void AddAdmin(int IDResult)
         {
-
+            string addAdminString = "INSERT INTO Admin (adminID) VALUES (" + IDResult + ");";
+            SqlCommand cmdAddAdmin = new SqlCommand();
+            cmdAddAdmin.Connection = Lab1DBConnection;
+            cmdAddAdmin.Connection.ConnectionString = Lab1DBConnString;
+            cmdAddAdmin.CommandText = addAdminString;
+            Lab1DBConnection.Open();
+            Lab1DBConnection.Close();
         }
 
         public static SqlDataReader ViewAllGrants()
@@ -137,15 +192,15 @@ namespace Lab1.Pages.DB
 
             SqlDataReader facultyReader = cmdGetFaculty.ExecuteReader();
 
-            int result = 0;
+            int facultyResult = 0;
             if (facultyReader.Read())
             {
-                result = Convert.ToInt32(facultyReader["facultyID"]);
+                facultyResult = Convert.ToInt32(facultyReader["facultyID"]);
             }
             Lab1DBConnection.Close();
 
             string AddProjectString = "INSERT INTO PROJECT (grantID, employeeID, adminID, facultyID, name, dueDate) VALUES (" +
-                project.grantID + "," + project.employeeID + "," + "12" + "," + result + "," + project.name + "," + project.DueDate + ");";
+                project.grantID + "," + project.employeeID + "," + "12" + "," + facultyResult + "," + project.name + "," + project.DueDate + ");";
 
             SqlCommand cmdAddProject = new SqlCommand();
             cmdAddProject.Connection = Lab1DBConnection;
