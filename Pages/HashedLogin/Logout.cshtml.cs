d1e09deb1cf8501d8d20a65167330b82073ee92a
lfisher2025 <soccerfish03@gmail.com>
2025-03-03 20:51:53 -0500

    fixing logout redirect

--- Pages/HashedLogin/Logout.cshtml.cs
+++ Pages/HashedLogin/Logout.cshtml.cs
@@ -8,7 +8,7 @@ namespace Lab1.Pages.HashedLogin
         public IActionResult OnGet()
         {
             HttpContext.Session.Clear(); // Clear session to remove login variables
-            return RedirectToPage("/HashedLogin"); // Redirect to login page
+            return RedirectToPage("/HashedLogin/HashedLogin"); // Redirect to login page
         }
     }
 }
